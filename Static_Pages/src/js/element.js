98848d9f20cb6b145ebf9209f13fbd1249d5c571
Yanchao Wang <wyc935398521@gmail.com>
2022-03-30 13:51:11 -0400

    a

--- Static_Pages/src/js/element.js
+++ Static_Pages/src/js/element.js
@@ -1,8 +1,19 @@
-const app = Vue.createApp({
+import Vue from "vue";
+import VueCardStack from "vue-card-stack";
+
+export default {
+    components: {
+        VueCardStack,
+    },
     data() {
         return {
-            visible: false
-        }
-    }
-})
-app.mount('#app')
+            cards: [
+                { background: "#00659d" },
+                { background: "#00abbc" },
+                { background: "#e2c58a" },
+                { background: "#fc8890" },
+                { background: "#b35d7f" },
+            ],
+        };
+    },
+};
